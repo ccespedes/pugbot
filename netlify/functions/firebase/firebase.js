c267ac6b6e02da96e630ff518859109f90177326
Carlos Cespedes <cespedes88@gmail.com>
2023-12-11 22:56:16 -0800

    change to post only on firebase function

--- netlify/functions/firebase/firebase.js
+++ netlify/functions/firebase/firebase.js
@@ -2,6 +2,15 @@ const databaseUrl = process.env.DATABASE_URL
 const projectId = process.env.PROJECT_ID
 
 const handler = async (event) => {
+  if (event.httpMethod !== 'POST') {
+    return {
+      statusCode: 405, // Method Not Allowed
+      body: JSON.stringify({
+        error: 'Invalid request method.',
+      }),
+    }
+  }
+
   return {
     statusCode: 200,
     body: JSON.stringify({
